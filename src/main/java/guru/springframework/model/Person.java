c1cb0bafb91b1f3b76cce08756537f274a601a8c
Admin <Admin@DESKTOP-4PE8HG5>
2019-12-18 15:36:37 +0530

    adding POJOs. Closes #7.

---
+++ src/main/java/guru/springframework/model/Person.java
@@ -0,0 +1,14 @@
+package guru.springframework.model;
+
+public class Person {
+	//Added firstname t
+	private String firstName;
+	private String lastName;
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;  
+	}
+	
+}
