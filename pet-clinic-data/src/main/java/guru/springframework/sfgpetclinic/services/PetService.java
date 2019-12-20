c7a568ef89ca7842044ad132aa8a53a7abd63554
Manjula <manjula.muttagi21@gmail.com>
2019-12-20 16:45:12 +0530

    commit for create pet clinic controllers. Closes #3.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,11 +1,6 @@
 package guru.springframework.sfgpetclinic.services;
 
-import java.util.Set;
-
 import guru.springframework.sfgpetclinic.model.Pet;
 
-public interface PetService {
-	Pet findById(Long id);
-	Pet Save(Pet pet);
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
