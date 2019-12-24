278ad67d33e6b60692639ea0418e5f0297300475
Manjula <manjula.muttagi21@gmail.com>
2019-12-24 16:40:11 +0530

    Commit for Create Vet Speciality Entity, associate to Vet. Closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
 
+public class Vet extends Person{
+	private Set<Speciality> specialites;
+
+	public Set<Speciality> getSpecialites() {
+		return specialites;
+	}
+
+	public void setSpecialites(Set<Speciality> specialites) {
+		this.specialites = specialites;
+	}
+
+	
+	
 }
